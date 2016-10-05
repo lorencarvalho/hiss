9e3bd429dbd846cb4f8d920b58aafdad6e9424b8
Loren Carvalho <lcarvalho@linkedin.com>
2016-10-04 23:31:11 -0700

    Initial commit

---
+++ hiss/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Loren Carvalho'
+__email__ = 'me@loren.pizza'
+__version__ = '0.1.0'
---
+++ setup.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+import sys
+import setuptools
+import subprocess
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+if sys.version_info[0:2] == (2, 6):
+    ipython_version = '<=0.11'
+else:
+    ipython_version = '>=5'
+
+requirements = [
+    'Click>=6.0',
+    'iPython{0}'.format(ipython_version),
+]
+
+
+class Venv(setuptools.Command):
+    user_options = []
+
+    def initialize_options(self):
+        """Abstract method that is required to be overwritten"""
+
+    def finalize_options(self):
+        """Abstract method that is required to be overwritten"""
+
+    def run(self):
+        venv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'venv', 'hiss')
+        venv_cmd = [
+            'virtualenv',
+            venv_path
+        ]
+        print('Creating virtual environment in ', venv_path)
+        subprocess.check_call(venv_cmd)
+        print('Linking `activate` to top level of project.\n')
+        print('To activate, simply run `source activate`.')
+        try:
+            os.symlink(
+                os.path.join(venv_path, 'bin', 'activate'),
+                os.path.join(os.path.dirname(os.path.abspath(__file__)), 'activate')
+            )
+        except OSError:
+            print('Unable to create symlink, you may have a stale symlink from a previous invocation.')
+
+
+setuptools.setup(
+    name='hiss',
+    version='0.1.0',
+    description="A simple and easily configured iPython-based python repl",
+    long_description=readme,
+    author="Loren Carvalho",
+    author_email='me@loren.pizza',
+    url='https://github.com/sixninetynine/hiss',
+    packages=[
+        'hiss',
+    ],
+    package_dir={'hiss':
+                 'hiss'},
+    entry_points={
+        'console_scripts': [
+            'hiss=hiss.cli:main'
+        ]
+    },
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords='hiss',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    cmdclass={'venv': Venv},
+)
