929b72a580cc408e9cfd7d72c5619ad679d0d6f2
Loren Carvalho <lcarvalho@linkedin.com>
2017-01-24 10:33:28 -0600

    do reload of pkg_resources if venv detected

--- src/hiss/cli/virtualenv.py
+++ src/hiss/cli/virtualenv.py
@@ -1,6 +1,8 @@
 import os
 import site
 
+from .magic import HissMagics
+
 
 def load_venv(python_version):
     if 'VIRTUAL_ENV' in os.environ:
@@ -12,4 +14,5 @@ def load_venv(python_version):
         )
         if os.path.exists(virtual_env):
             site.addsitedir(virtual_env)
+            HissMagics().reload_pkg_resources('')
             print("\x1B[3mvirtualenv detected -> {}\x1B[23m".format(virtual_env))
